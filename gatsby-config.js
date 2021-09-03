8a2fd2f2d37836444fb774d615e9e8d85587724f
nglvnyls <nglvnyls@pm.me>
2021-09-03 08:22:00 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "my-first-gatsby-site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 // styles
 const paragraphStyles = {
@@ -72,6 +73,14 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home page">
       <p style={paragraphStyles}>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+      <StaticImage
+        alt="caricatura de l'alain"
+        src="../images/alain1.jpg"
+      />
     </Layout>
   )
 }
